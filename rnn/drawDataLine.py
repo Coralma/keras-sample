9fb922ad3f05952fa033aed19a3d184bcda8856f
DESKTOP-955G2G9\ccc <qma@cccis.com>
2017-11-15 18:05:02 +0800

    Add data line sample

---
+++ rnn/drawDataLine.py
@@ -0,0 +1,47 @@
+# 趋势图绘制，根据数据来绘制一个增长曲线
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+from keras.models import Sequential
+from keras.layers import Dense
+import matplotlib.pyplot as plt
+
+# create some data
+X = np.linspace(-1, 1, 200)
+np.random.shuffle(X)    # randomize the data
+Y = 0.5 * X + 2 + np.random.normal(0, 0.05, (200, ))
+# plot data
+plt.scatter(X, Y)
+plt.show()
+
+X_train, Y_train = X[:160], Y[:160]     # first 160 data points
+X_test, Y_test = X[160:], Y[160:]       # last 40 data points
+
+# build a neural network from the 1st layer to the last layer
+# 创建一个Sequential表示按顺序的model（模型）
+model = Sequential()
+# 在model中加入一个Dense全连接层
+model.add(Dense(units=1, input_dim=1))
+
+# choose loss function and optimizing method
+# 通过mse（二次方的误差），采用sgd（乱序）优化器
+model.compile(loss='mse', optimizer='sgd')
+
+# training
+print('Training -----------')
+for step in range(301):
+    cost = model.train_on_batch(X_train, Y_train)
+    if step % 100 == 0:
+        print('train cost: ', cost)
+
+# test
+print('\nTesting ------------')
+cost = model.evaluate(X_test, Y_test, batch_size=40)
+print('test cost:', cost)
+W, b = model.layers[0].get_weights()
+print('Weights=', W, '\nbiases=', b)
+
+# plotting the prediction
+Y_pred = model.predict(X_test)
+plt.scatter(X_test, Y_test)
+plt.plot(X_test, Y_pred)
+plt.show()
